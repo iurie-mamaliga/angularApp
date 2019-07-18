50e4659d7cbb83553fea4b2b487e5730e5b7d6c3
Iurie Mamaliga <coding.rescue@gmail.com>
2019-07-18 11:48:42 -0400

    message

---
+++ src/app/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public street: string,
+        public street2: string,
+        public city: string,
+        public state: string,
+        public zipCode: number,
+        public email: string,
+        public phone: number,
+        public age: string
+    ) {}
+}
